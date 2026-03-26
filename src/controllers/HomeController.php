ecabbd8c6c7fd5428578a557128eaf66da82cf04
MattiaP7 <7mattiapirazzi@gmail.com>
2026-03-26 18:39:03 +0100

    Update innocuo

--- src/controllers/HomeController.php
+++ src/controllers/HomeController.php
@@ -5,8 +5,6 @@ class HomeController
 {
   public function index()
   {
-    $page_title = 'Home Page';
-
     include __DIR__ . "/../views/layout/header.php";
     include __DIR__ . "/../views/home/index.php";
     include __DIR__ . "/../views/layout/footer.php";
